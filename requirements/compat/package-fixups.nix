6ced6138843caca18fece62f74ad8820daaf7bba
Oleg Blyednov <oleg@blyednov.dev>
2025-08-30 23:01:54 -0600

    maybe fix rocm

--- requirements/compat/package-fixups.nix
+++ requirements/compat/package-fixups.nix
@@ -8,196 +8,216 @@
   python,
   pythonPkgs,
   lib,
-}:
-final: prev:
-let
+}: final: prev: let
   hipblaslt = pkgs.callPackage ./hipblaslt {
     inherit python;
     inherit pythonPkgs;
   };
 
   # Add dependencies to a package
-  withExtraDependencies =
-    pkg: extraDeps:
+  withExtraDependencies = pkg: extraDeps:
     pkg.overridePythonAttrs (prev: {
       dependencies = prev.dependencies ++ extraDeps;
     });
 
   # Add zlib to a package
-  withZlib = pkg: withExtraDependencies pkg [ pkgs.zlib ];
+  withZlib = pkg: withExtraDependencies pkg [pkgs.zlib];
 
   # Hook for removing all compiled bytecode
   pythonRemoveBytecodeHook = pythonPkgs.callPackage (
-    { makePythonHook }:
-    makePythonHook {
-      name = "python-remove-bytecode-hook";
-      propagatedBuildInputs = [ ];
-    } ./python-remove-bytecode-hook.sh
-  ) { };
+    {makePythonHook}:
+      makePythonHook {
+        name = "python-remove-bytecode-hook";
+        propagatedBuildInputs = [];
+      }
+      ./python-remove-bytecode-hook.sh
+  ) {};
 
   # Hook for copying libraries to lib output
   pythonPropagateLibHook = pythonPkgs.callPackage (
-    { makePythonHook }:
-    makePythonHook {
-      name = "python-propagate-lib-hook";
-      propagatedBuildInputs = [ ];
-      substitutions = {
-        pythonSitePackages = python.sitePackages;
-      };
-    } ./python-propagate-lib-hook.sh
-  ) { };
+    {makePythonHook}:
+      makePythonHook {
+        name = "python-propagate-lib-hook";
+        propagatedBuildInputs = [];
+        substitutions = {
+          pythonSitePackages = python.sitePackages;
+        };
+      }
+      ./python-propagate-lib-hook.sh
+  ) {};
 
   # Remove all bytecode from the package
-  removePythonBytecode =
-    pkg:
+  removePythonBytecode = pkg:
     pkg.overridePythonAttrs (prev: {
-      nativeBuildInputs = prev.nativeBuildInputs ++ [ pythonRemoveBytecodeHook ];
+      nativeBuildInputs = prev.nativeBuildInputs ++ [pythonRemoveBytecodeHook];
     });
 
   # Make sure the package has a lib output
-  propagateLib =
-    pkg:
+  propagateLib = pkg:
     pkg.overrideAttrs (prev: {
-      outputs = (prev.outputs or [ ]) ++ [ "lib" ];
-      nativeBuildInputs = prev.nativeBuildInputs ++ [ pythonPropagateLibHook ];
+      outputs = (prev.outputs or []) ++ ["lib"];
+      nativeBuildInputs = prev.nativeBuildInputs ++ [pythonPropagateLibHook];
+    });
+in
+  with pkgs; rec {
+    inherit hipblaslt;
+
+    # Numpy needs zlib and also needs to define coreIncludeDir so that scipy
+    # can consume it
+    numpy = prev.numpy.overridePythonAttrs (prevPyAttrs: {
+      dependencies = prevPyAttrs.dependencies ++ [zlib];
+      passthru =
+        (prevPyAttrs.passthru or {})
+        // {
+          # Needed for nixpkgs scipy to build
+          coreIncludeDir = "${final.numpy}/${python.sitePackages}/numpy/core/include";
+        };
     });
 
-in
-with pkgs;
-rec {
-  inherit hipblaslt;
+    # A bunch of packages require zlib
+    llvmlite = withZlib prev.llvmlite;
+    tokenizers = withZlib prev.tokenizers;
+    pillow = withZlib prev.pillow;
+    av = withZlib prev.av;
+    triton = withZlib (
+      prev.triton.overridePythonAttrs (prev: {
+        # https://github.com/NixOS/nixpkgs/issues/96654
+        dontStrip = 1;
+      })
+    );
 
-  # Numpy needs zlib and also needs to define coreIncludeDir so that scipy
-  # can consume it
-  numpy = prev.numpy.overridePythonAttrs (prevPyAttrs: {
-    dependencies = prevPyAttrs.dependencies ++ [ zlib ];
-    passthru = (prevPyAttrs.passthru or { }) // {
-      # Needed for nixpkgs scipy to build
-      coreIncludeDir = "${final.numpy}/${python.sitePackages}/numpy/core/include";
-    };
-  });
+    # Random other dependencies
+    opencv-python = withExtraDependencies prev.opencv-python [
+      libGL
+      glib
 
-  # A bunch of packages require zlib
-  llvmlite = withZlib prev.llvmlite;
-  tokenizers = withZlib prev.tokenizers;
-  pillow = withZlib prev.pillow;
-  av = withZlib prev.av;
-  triton = withZlib (
-    prev.triton.overridePythonAttrs (prev: {
-      # https://github.com/NixOS/nixpkgs/issues/96654
-      dontStrip = 1;
-    })
-  );
+      xorg.libxcb
+      xorg.libICE
+      xorg.libSM
+    ];
 
-  # Random other dependencies
-  opencv-python = withExtraDependencies prev.opencv-python [
-    libGL
-    glib
+    # Cuda stuff
+    torch = propagateLib (
+      prev.torch.overridePythonAttrs (prev: {
+        # Will be added by pkgs.autoAddDriverRunpath
+        autoPatchelfIgnoreMissingDeps = ["libcuda.so.1"];
+        nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [pkgs.autoAddDriverRunpath];
 
-    xorg.libxcb
-    xorg.libICE
-    xorg.libSM
-  ];
+        # Additional dependencies for ROCm 6.4 wheels
+        dependencies =
+          (prev.dependencies or [])
+          ++ [
+            # Compression libraries required by ROCm 6.4
+            pkgs.zlib # libz.so.1
+            pkgs.zstd # libzstd.so.1
+            pkgs.xz # liblzma.so.5
+            pkgs.bzip2 # libbz2.so.1
 
-  # Cuda stuff
-  torch = propagateLib (
-    prev.torch.overridePythonAttrs (prev: {
-      # Will be added by pkgs.autoAddDriverRunpath
-      autoPatchelfIgnoreMissingDeps = [ "libcuda.so.1" ];
-      nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++ [ pkgs.autoAddDriverRunpath ];
+            # ROCm libraries for AMD GPU support
+            pkgs.rocmPackages.rocblas # librocblas.so.4
+            pkgs.rocmPackages.rocm-runtime # Additional ROCm runtime support
+          ];
+      })
+    );
 
-      # TODO: This is ROCm only!
-      # dependencies = (prev.dependencies or []) ++ [ hipblaslt ];
-    })
-  );
+    torchaudio = propagateLib (
+      prev.torchaudio.overridePythonAttrs (prev: {
+        dependencies = [
+          pkgs.ffmpeg_6
+          pkgs.sox
+          torch
+        ];
 
-  torchaudio = propagateLib (
-    prev.torchaudio.overridePythonAttrs (prev: {
-      dependencies = [
-        pkgs.ffmpeg_6
-        pkgs.sox
-        torch
-      ];
+        # Torchaudio automatically selects between ffmpeg 6, 5 and 4 -
+        # we provide 6, so ignore the missing ffmpeg 4 and 5
+        autoPatchelfIgnoreMissingDeps = [
+          # ffmpeg 5
+          "libavutil.so.57"
+          "libavcodec.so.59"
+          "libavformat.so.59"
+          "libavfilter.so.8"
+          "libavutil.so.57"
+          "libavdevice.so.59"
 
-      # Torchaudio automatically selects between ffmpeg 6, 5 and 4 -
-      # we provide 6, so ignore the missing ffmpeg 4 and 5
-      autoPatchelfIgnoreMissingDeps = [
-        # ffmpeg 5
-        "libavutil.so.57"
-        "libavcodec.so.59"
-        "libavformat.so.59"
-        "libavfilter.so.8"
-        "libavutil.so.57"
-        "libavdevice.so.59"
+          # ffmpeg 4
+          "libavutil.so.56"
+          "libavcodec.so.58"
+          "libavformat.so.58"
+          "libavfilter.so.7"
+          "libavutil.so.56"
+          "libavdevice.so.58"
+        ];
+      })
+    );
 
-        # ffmpeg 4
-        "libavutil.so.56"
-        "libavcodec.so.58"
-        "libavformat.so.58"
-        "libavfilter.so.7"
-        "libavutil.so.56"
-        "libavdevice.so.58"
-      ];
-    })
-  );
+    bitsandbytes = (
+      prev.bitsandbytes.overridePythonAttrs (prev: {
+        # Available at runtime if, and only if, CUDA is loaded -
+        # but also only required if its loaded either way, so we
+        # ignore these dependencies
+        autoPatchelfIgnoreMissingDeps = [
+          # CUDA 11.x dependencies (original)
+          "libcudart.so.11.0"
+          "libcublas.so.11"
+          "libcusparse.so.11"
+          "libcublasLt.so.11"
 
-  bitsandbytes = (
-    prev.bitsandbytes.overridePythonAttrs (prev: {
-      # Available at runtime if, and only if, CUDA is loaded -
-      # but also only required if its loaded either way, so we
-      # ignore these dependencies
-      autoPatchelfIgnoreMissingDeps = [
-        "libcudart.so.11.0"
-        "libcublas.so.11"
-        "libcusparse.so.11"
-        "libcublasLt.so.11"
-      ];
-    })
-  );
+          # NOTE: CUDA 12.x dependencies for newer bitsandbytes versions
+          # These are not available on AMD ROCm systems but are optional
+          "libcudart.so.12"
+          "libcublas.so.12"
+          "libcusparse.so.12"
+          "libcublasLt.so.12"
+        ];
+      })
+    );
 
-  numba = withExtraDependencies prev.numba (let
-    tbb = if pkgs ? tbb_2021 then pkgs.tbb_2021 else pkgs.tbb_2021_11;
-  in [
-    tbb
-  ]);
+    numba = withExtraDependencies prev.numba (let
+      tbb =
+        if pkgs ? tbb_2021
+        then pkgs.tbb_2021
+        else pkgs.tbb_2021_11;
+    in [
+      tbb
+    ]);
 
-  filterpy = prev.filterpy.overridePythonAttrs (prev: {
-    # Fails for some reason
-    doCheck = false;
-  });
+    filterpy = prev.filterpy.overridePythonAttrs (prev: {
+      # Fails for some reason
+      doCheck = false;
+    });
 
-  jsonmerge = prev.jsonmerge.overridePythonAttrs (prev: {
-    # No idea either, 2 tests fail
-    doCheck = false;
-  });
+    jsonmerge = prev.jsonmerge.overridePythonAttrs (prev: {
+      # No idea either, 2 tests fail
+      doCheck = false;
+    });
 
-  # Bytecode removal (thanks NVIDIA for shipping libraries with overlapping bytecode..)
-  nvidia-nvjitlink-cu12 = propagateLib (removePythonBytecode prev.nvidia-nvjitlink-cu12);
-  nvidia-cusparse-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusparse-cu12);
-  nvidia-cusparselt-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusparselt-cu12);
-  nvidia-cusolver-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusolver-cu12);
-  nvidia-cudnn-cu12 = propagateLib (removePythonBytecode (withZlib prev.nvidia-cudnn-cu12));
-  nvidia-cuda-cupti-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-cupti-cu12);
-  nvidia-cublas-cu12 = propagateLib (removePythonBytecode prev.nvidia-cublas-cu12);
-  nvidia-cuda-nvrtc-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-nvrtc-cu12);
-  nvidia-cuda-runtime-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-runtime-cu12);
-  nvidia-curand-cu12 = propagateLib (removePythonBytecode prev.nvidia-curand-cu12);
-  nvidia-cufft-cu12 = propagateLib (removePythonBytecode prev.nvidia-cufft-cu12);
-  nvidia-nccl-cu12 = propagateLib (removePythonBytecode prev.nvidia-nccl-cu12);
-  nvidia-nvtx-cu12 = propagateLib (removePythonBytecode prev.nvidia-nvtx-cu12);
-  nvidia-cufile-cu12 = propagateLib (
-    withExtraDependencies (removePythonBytecode prev.nvidia-cufile-cu12) [
-      pkgs.rdma-core
-    ]
-  );
+    # Bytecode removal (thanks NVIDIA for shipping libraries with overlapping bytecode..)
+    nvidia-nvjitlink-cu12 = propagateLib (removePythonBytecode prev.nvidia-nvjitlink-cu12);
+    nvidia-cusparse-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusparse-cu12);
+    nvidia-cusparselt-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusparselt-cu12);
+    nvidia-cusolver-cu12 = propagateLib (removePythonBytecode prev.nvidia-cusolver-cu12);
+    nvidia-cudnn-cu12 = propagateLib (removePythonBytecode (withZlib prev.nvidia-cudnn-cu12));
+    nvidia-cuda-cupti-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-cupti-cu12);
+    nvidia-cublas-cu12 = propagateLib (removePythonBytecode prev.nvidia-cublas-cu12);
+    nvidia-cuda-nvrtc-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-nvrtc-cu12);
+    nvidia-cuda-runtime-cu12 = propagateLib (removePythonBytecode prev.nvidia-cuda-runtime-cu12);
+    nvidia-curand-cu12 = propagateLib (removePythonBytecode prev.nvidia-curand-cu12);
+    nvidia-cufft-cu12 = propagateLib (removePythonBytecode prev.nvidia-cufft-cu12);
+    nvidia-nccl-cu12 = propagateLib (removePythonBytecode prev.nvidia-nccl-cu12);
+    nvidia-nvtx-cu12 = propagateLib (removePythonBytecode prev.nvidia-nvtx-cu12);
+    nvidia-cufile-cu12 = propagateLib (
+      withExtraDependencies (removePythonBytecode prev.nvidia-cufile-cu12) [
+        pkgs.rdma-core
+      ]
+    );
 
-  # ROCm specific stuff
-  pytorch-triton-rocm = withExtraDependencies prev.pytorch-triton-rocm [
-    pkgs.zlib
-    pkgs.zstd
-  ];
+    # ROCm specific stuff
+    pytorch-triton-rocm = withExtraDependencies prev.pytorch-triton-rocm [
+      pkgs.zlib
+      pkgs.zstd
+    ];
 
-  # Extra packages
-  webui-python-raw = python;
-  webui-python-env = python.withPackages (_: final.allRequirements);
-}
+    # Extra packages
+    webui-python-raw = python;
+    webui-python-env = python.withPackages (_: final.allRequirements);
+  }
--- source/forge/default.nix
+++ source/forge/default.nix
@@ -1,21 +1,25 @@
-{ pkgs
-, mkWebuiDistrib
-, ...
-}:
-let
+{
+  pkgs,
+  mkWebuiDistrib,
+  ...
+}: let
   raw = pkgs.callPackage ./raw.nix {};
 
   createPackage = import ./package.nix;
-in
-{
+in {
   cuda = mkWebuiDistrib {
     source = raw;
     python = pkgs.python311;
 
-    additionalRequirements = raw.additionalRequirements ++ [
-      # Acceleration on CUDA
-      { name = "xformers"; spec = "0.0.27"; }
-    ];
+    additionalRequirements =
+      raw.additionalRequirements
+      ++ [
+        # Acceleration on CUDA
+        {
+          name = "xformers";
+          spec = "0.0.27";
+        }
+      ];
 
     installInstructions = ./install-instructions-cuda.json;
 
@@ -25,15 +29,26 @@ in
   rocm = mkWebuiDistrib {
     source = raw;
     python = pkgs.python311;
-    additionalRequirements = raw.additionalRequirements ++ [
-      { name = "torch"; spec = "https://download.pytorch.org/whl/nightly/rocm6.0/torch-2.5.0.dev20240802%2Brocm6.0-cp310-cp310-linux_x86_64.whl"; }
-      { name = "torchvision"; spec = "https://download.pytorch.org/whl/nightly/rocm6.0/torchvision-0.20.0.dev20240822%2Brocm6.0-cp310-cp310-linux_x86_64.whl"; }
-    ];
-    additionalPipArgs = ["--extra-index-url" "https://download.pytorch.org/whl/nightly/rocm6.2/"];
+    additionalRequirements =
+      raw.additionalRequirements
+      ++ [
+        # NOTE: Use specific nightly PyTorch with ROCm 6.4 for RX 9000 series support
+        {
+          name = "torch";
+          spec = "2.9.0.dev20250827+rocm6.4"; # Latest available ROCm 6.4 nightly
+        }
+        {
+          name = "torchvision";
+          spec = "0.24.0.dev20250827+rocm6.4"; # Compatible torchvision version
+        }
+        # TODO: Re-enable triton once build issues are resolved
+        # Triton provides GPU kernel optimizations but isn't required for basic functionality
+      ];
+    additionalPipArgs = ["--extra-index-url" "https://download.pytorch.org/whl/nightly/rocm6.4/"];
 
     installInstructions = ./install-instructions-rocm.json;
 
-    createPackage = throw "ROCm is currently broken";
-    # inherit createPackage; # Want to work on ROCm? Swap the line above with this
+    # createPackage = throw "ROCm is currently broken";
+    inherit createPackage; # Want to work on ROCm? Swap the line above with this
   };
 }
